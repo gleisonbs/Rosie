17c5c81c368dab4d37bd4658f9e8a6186434ea5a
Gleison Batista Soares <gleisonbs@gmail.com>
2021-04-24 15:51:33 -0300

    feat: ensure get_intent_by_token returns most similar intent

--- rosie/chatbot.py
+++ rosie/chatbot.py
@@ -1,17 +1,32 @@
+from rosie.nlp_engine import NLPEngine
+
+
 class Chatbot:
     def __init__(self, name):
         self.name = name
         self.intents = {}
+        self.nlp = NLPEngine().pt
 
     def add_intent(self, name, phrases):
         self.intents[name] = phrases
+
+    def get_intent_by_token(self, token):
+        similarities = {}
+        largest = 0
+        for intent, phrases in self.intents.items():
+            max_intent_similarity = max(
+                [token.similarity(self.nlp(p)) for p in phrases]
+            )
+            similarities[max_intent_similarity] = intent
+            largest = max(largest, max_intent_similarity)
+        return similarities[largest]
 
     def __str__(self):
         if len(self.intents) == 0:
             return "{}"
 
         response = ""
-        for k, v in self.intents.items():
-            response += f"intent:{k}\n"
-            response += "".join([f" - {p}\n" for p in v])
+        for intent, phrases in self.intents.items():
+            response += f"intent:{intent}\n"
+            response += "".join([f" - {phrase}\n" for phrase in phrases])
         return response
--- tests/test_chatbot.py
+++ tests/test_chatbot.py
@@ -1,9 +1,14 @@
 from unittest import TestCase
 
 from rosie.chatbot import Chatbot
+from rosie.nlp_engine import NLPEngine
 
 
 class TestChatbot(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.nlp = NLPEngine().pt
+
     def setUp(self):
         self.chatbot = Chatbot("test")
 
@@ -19,24 +24,41 @@ class TestChatbot(TestCase):
             "Chatbot: no name attribute found",
         )
 
-    def test_has_all_methods(self):
+    def test_has_add_intent_method(self):
         self.assertEqual(
             hasattr(self.chatbot, "add_intent"),
             True,
             "Chatbot: no add_intent method found",
         )
-
         add_intent = self.chatbot.add_intent
         self.assertEqual(
             hasattr(add_intent, "__call__"),
             True,
             "Chatbot: add_intent is not a method",
         )
-
         self.assertEqual(
             add_intent.__code__.co_argcount,
             1 + 2,
             "Chatbot: add_intent should accept two parameters (name, phrases)",
+        )
+
+    def test_has_get_intent_by_token_method(self):
+        self.assertEqual(
+            hasattr(self.chatbot, "get_intent_by_token"),
+            True,
+            "Chatbot: no get_intent_by_token method found",
+        )
+        get_intent_by_token = self.chatbot.get_intent_by_token
+        self.assertEqual(
+            hasattr(get_intent_by_token, "__call__"),
+            True,
+            "Chatbot: get_intent_by_token is not a method",
+        )
+        self.assertEqual(
+            get_intent_by_token.__code__.co_argcount,
+            1 + 1,
+            "Chatbot: get_intent_by_token should accept"
+            "two parameters (name, phrases)",
         )
 
     def test_correctly_adds_new_intent(self):
@@ -54,6 +76,41 @@ class TestChatbot(TestCase):
             "Chatbot: add_intents failed to add to intents",
         )
 
+    def test_correctly_returns_most_similar_intent(self):
+        self.chatbot.add_intent(
+            "price",
+            ["qual o valor", "quanto custa", "preço", "me fala o total"],
+        )
+        self.chatbot.add_intent(
+            "greet",
+            [
+                "oi",
+                "olá",
+                "tudo bem",
+                "como vai",
+                "opa",
+                "bom dia",
+                "boa noite",
+                "boa tarde",
+            ],
+        )
+
+        token = TestChatbot.nlp("quanto custa")
+        most_similar_intent = self.chatbot.get_intent_by_token(token)
+        self.assertEqual(
+            most_similar_intent,
+            "price",
+            "Chatbot: get_intent_by_token failed to return correct intent",
+        )
+
+        token = TestChatbot.nlp("oi, tudo bem?")
+        most_similar_intent = self.chatbot.get_intent_by_token(token)
+        self.assertEqual(
+            most_similar_intent,
+            "greet",
+            "Chatbot: get_intent_by_token failed to return correct intent",
+        )
+
     def test_prints_correctly(self):
         self.assertEqual(str(self.chatbot), "{}")
 
@@ -69,7 +126,11 @@ intent:test_intent2
  - test5
  - test6
 """
-        self.assertEqual(str(self.chatbot), expected_str)
+        self.assertEqual(
+            str(self.chatbot),
+            expected_str,
+            "Chatbot: __str__ return incorrect formatted data",
+        )
 
     def test_can_initiate_correctly(self):
         chatbot = Chatbot("test_name")
