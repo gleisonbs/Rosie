c9618345c7b75b0e9d0f478dea2c46ee046801d0
Gleison Batista Soares <gleisonbs@gmail.com>
2021-08-22 16:58:05 -0300

    refactor: create examples folder

--- examples/pizzaria_pt.py
+++ examples/pizzaria_pt.py
@@ -1,13 +1,21 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from rosie.chatbot import Chatbot
 from rosie.nlp_engine import NLPEngine
 
+# Create the chatbot with the name "Pizzaria"
 chatbot = Chatbot(NLPEngine().pt, "Pizzaria")
 
+# Add the price intent
 chatbot.add_intent(
     "price",
     ["qual o valor", "quanto custa", "preço", "me fala o total", "comprar"],
 )
 
+# Add the greetings intent
 chatbot.add_intent(
     "greet",
     [
@@ -22,15 +30,17 @@ chatbot.add_intent(
     ],
 )
 
+# Add the soda intent
 chatbot.add_intent(
     "soda",
     [
-        "tem coca cola" "refrigerantes",
+        "tem coca cola",
+        "refrigerantes",
         "que refris vocês tem",
         "tem refrigerante 2l",
         "quais as opções de refrigerantes",
     ],
 )
 
-
+# Try to guess the intent by the phrase
 print(chatbot.get_intent_by_token("tem coca de 2l"))
